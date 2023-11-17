09dbf0e51fb4e9652a0bc4896f607c6b312b3f4f
tomaXx28 <oleax28@gmail.com>
2023-11-17 16:02:21 -0300

    cambio en la api
    ola

--- TestApi_Django/core/models.py
+++ TestApi_Django/core/models.py
@@ -2,10 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Alumno(models.Model):
-    idAlumno = models.IntegerField(primary_key=True, verbose_name = "Id de Usuario")
-    Gmail = models.CharField(max_length=50, verbose_name = "Gmail")
-    Contrasena = models.CharField(max_length=50, verbose_name = "Contrasena")
-    nombreAlumno = models.CharField(max_length=50, verbose_name = "Nombre de Usuario")
+    ROLES_CHOICES = [
+        ('alumno', 'Alumno'),
+        ('profesor', 'Profesor'),
+    ]
+
+    idAlumno = models.IntegerField(primary_key=True, verbose_name="Id de Usuario")
+    Gmail = models.CharField(max_length=50, verbose_name="Gmail")
+    Contrasena = models.CharField(max_length=50, verbose_name="Contrasena")
+    nombreAlumno = models.CharField(max_length=50, verbose_name="Nombre de Usuario")
+    rol = models.CharField(max_length=10, choices=ROLES_CHOICES, default='alumno', verbose_name="Rol")
 
     def __str__(self):
-        return self.nombreAlumno
+        return f"{self.nombreAlumno} - {self.rol}"
+
